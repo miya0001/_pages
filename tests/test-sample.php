deb1a693564ad3b779fb8cded306cfeefb244c03
miya0001 <miya@wpist.me>
2017-03-02 14:29:32 +0900

    add unit tests

---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Child_Pages_Shortcode
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
