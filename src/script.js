bb606727752496517a7c3606530f58462c641ded
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-03-31 01:01:54 +0900

    bug fix for msedge

--- src/script.js
+++ src/script.js
@@ -4,14 +4,15 @@
         var css = '/_pages/css/style.min.css';
 
         var scripts = document.querySelectorAll( 'script' );
-        scripts.forEach( function( item ) {
+        for ( var i = 0; i < scripts.length; i++ ) {
+            var item = scripts[ i ];
             var src = item.getAttribute( 'src' );
             if ( src ) {
                 if ( 0 < src.indexOf( me ) ) {
                     css = src.replace( me, css );
                 }
             }
-        } );
+        }
 
         var link = document.createElement( 'link' );
         link.setAttribute( 'rel', 'stylesheet' );
